c16b45da7036a497654110903ff142e93ec85d00
EC2 Default User <ch5cxcxxh@gmail.com>
2020-05-16 15:37:03 +0000

    finish searching

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -62,7 +62,12 @@ class UsersController < ApplicationController
   end
   
   def search
-    
+    @user_or_post = params[:option]
+    if @user_or_post == "1"
+      @users = User.search(params[:search], @user_or_post)
+    else
+      @posts = Post.search(params[:search], @user_or_post)
+    end
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    def User.search(search, user_or_post)
+        if user_or_post == "1"
+            User.where(['name LIKE ?', "%#{search}%"])
+        else
+            User.all
+        end
+    end
+    
 end
